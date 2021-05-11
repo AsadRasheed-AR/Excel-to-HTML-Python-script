2ee5db9b46d714fee9973a3c718651cc0d2c290f
AsadRasheed-AR <asxadlodhi@gmail.com>
2021-05-11 08:29:44 +0500

    requirement.txt added

--- Deliverable.py
+++ Deliverable.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[65]:
+# In[265]:
 
 
 import numpy as np
@@ -11,7 +11,14 @@ import re
 import bs4
 
 
-# In[66]:
+# In[266]:
+
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+
+# In[267]:
 
 
 #Row with plus sign
@@ -20,28 +27,33 @@ expand_next_row = '<tr class="MDrow1 expand-next-row"><td class="MDrow1"><span c
 expandable_row = '<tr class="expandable-row"><td class="MDrow1">'
 
 
-# In[67]:
+# In[268]:
 
 
 def getClassname(number_of_dec,next_number_of_dec,cellValue):
-    if ((number_of_dec == 0)):                                     #Whole Number
-        if (number_of_dec < next_number_of_dec):
-            return '<tr class="MDrow1 RowClass2 expand-next-row"><td class="MDrow1"><span class="plus-sign">+</span><span class="plus-sign" style="display: none;">–</span>'
-        return '<tr class="MDrow RowClass2"><td class="MDrow1">'
-    elif number_of_dec == 1:                                       #One Decimel
-        if (number_of_dec < next_number_of_dec):
-            return '<tr class="MDrow1 expandable-row expand-next-row RowClass3" style="display: none;"><td class="MDrow1"><span class="plus-sign" style="display: none;">+</span><span class="plus-sign">–</span>'
-        return '<tr class="expandable-row RowClass3" style="display: none;"><td class="MDrow1">'
-    elif number_of_dec == 2:                                       #Two Decimel
-        if (number_of_dec < next_number_of_dec):
-            return '<tr class="MDrow1 expandable-row expand-next-row RowClass4" style="display: none;"><td class="MDrow1"><span class="plus-sign" style="display: none;">+</span><span class="plus-sign">–</span>'
-        return '<tr class="expandable-row RowClass4" style="display: none;"><td class="MDrow4">'
-    elif number_of_dec == 3:                                       #Three Decimel
-        if (number_of_dec < next_number_of_dec):
-            return '<tr class="MDrow1 expandable-row expand-next-row RowClass5" style="display: none;"><td class="MDrow1"><span class="plus-sign" style="display: none;">+</span><span class="plus-sign">–</span>'
-        return '<tr class="expandable-row RowClass5" style="display: none;"><td class="RowClass5">'
-    else:
-        return '<tr class="">'
+#     try:
+#         val = float(cellValue)
+    if(hasNumbers(cellValue)):
+        if ((number_of_dec == 0)):                                     #Whole Number
+            if (number_of_dec < next_number_of_dec):
+                return '<tr class="MDrow1 RowClass2 expand-next-row"><td class="MDrow1"><span class="plus-sign">+</span><span class="plus-sign" style="display: none;">–</span>'
+            return '<tr class="MDrow RowClass2"><td class="MDrow1">'
+        elif number_of_dec == 1:                                       #One Decimel
+            if (number_of_dec < next_number_of_dec):
+                return '<tr class="MDrow1 expandable-row expand-next-row RowClass3" style="display: none;"><td class="MDrow1"><span class="plus-sign" style="display: none;">+</span><span class="plus-sign">–</span>'
+            return '<tr class="expandable-row RowClass3" style="display: none;"><td class="MDrow1">'
+        elif number_of_dec == 2:                                       #Two Decimel
+            if (number_of_dec < next_number_of_dec):
+                return '<tr class="MDrow1 expandable-row expand-next-row RowClass4" style="display: none;"><td class="MDrow1"><span class="plus-sign" style="display: none;">+</span><span class="plus-sign">–</span>'
+            return '<tr class="expandable-row RowClass4" style="display: none;"><td class="MDrow4">'
+        elif number_of_dec == 3:                                       #Three Decimel
+            if (number_of_dec < next_number_of_dec):
+                return '<tr class="MDrow1 expandable-row expand-next-row RowClass5" style="display: none;"><td class="MDrow1"><span class="plus-sign" style="display: none;">+</span><span class="plus-sign">–</span>'
+            return '<tr class="expandable-row RowClass5" style="display: none;"><td class="RowClass5">'
+        else:
+            return '<tr class="">'
+    else:                                                            #Header Row
+        return f'<tr class="Header1row RowClass2"><td class="Header1row">{cellValue}'
 
 
 # #Function to determine the type of row
@@ -61,7 +73,7 @@ def getClassname(number_of_dec,next_number_of_dec,cellValue):
 #         return f'<tr class="Header1row RowClass1"><td class="Header1row">{cellValue}'
 
 
-# In[68]:
+# In[269]:
 
 
 #Read orignal html file, for embedding new table
@@ -70,25 +82,25 @@ with open(file_path) as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
 
-# In[69]:
+# In[270]:
 
 
 #orignal html
 soup.table
 
 
-# In[70]:
+# In[271]:
 
 
 #Read csv to create html table
-df = pd.read_csv('LstMetadataprofilCopy.csv',header=None)
+df = pd.read_csv('LstMetadataprofiltest.csv',header=None,encoding = "ISO-8859-1")
 df = df[[0,1,33,34,35]]
 df = df.dropna(how='all')
 df = df.reset_index(drop=True)
 df.head(10)
 
 
-# In[71]:
+# In[272]:
 
 
 #Choose columns with data and ignore the class column
@@ -96,13 +108,13 @@ sub_set_df_fhtml = df[[1,33,34,35]]
 sub_set_df_fhtml.head(10)
 
 
-# In[72]:
+# In[273]:
 
 
 len(sub_set_df_fhtml[1])
 
 
-# In[73]:
+# In[274]:
 
 
 #Replace nan values with " "
@@ -110,7 +122,7 @@ sub_set_df_fhtml = sub_set_df_fhtml.replace(np.nan, '', regex=True)
 sub_set_df_fhtml.head(10)
 
 
-# In[74]:
+# In[275]:
 
 
 # New Html Table
@@ -118,7 +130,7 @@ html = sub_set_df_fhtml.to_html(index=False,header=False)
 html
 
 
-# In[75]:
+# In[276]:
 
 
 #Create a parser to parse newly created table for adding custom classes
@@ -126,13 +138,13 @@ new_soup = BeautifulSoup(html,"html.parser")
 tags = new_soup.find_all('tr',recursive=True)
 
 
-# In[76]:
+# In[277]:
 
 
 new_soup
 
 
-# In[77]:
+# In[278]:
 
 
 #Iterate over each row and check which class it belongs to and then make necessary changes
@@ -154,7 +166,7 @@ for num , tag in enumerate(tags):
         print(tags[num])
 
 
-# In[78]:
+# In[279]:
 
 
 #remove rows to add new rows with modifications
@@ -162,7 +174,7 @@ for tr in new_soup.find_all("tr"):
     tr.decompose()
 
 
-# In[79]:
+# In[280]:
 
 
 #Add new rows in table with modifications (Custom classes)
@@ -170,13 +182,13 @@ for tag in tags:
     new_soup.tbody.append(tag)
 
 
-# In[80]:
+# In[281]:
 
 
 new_soup.tbody
 
 
-# In[81]:
+# In[282]:
 
 
 #remove rows to add new rows with modifications
@@ -184,7 +196,7 @@ for tr in soup.find_all("tr"):
     tr.decompose()
 
 
-# In[82]:
+# In[283]:
 
 
 #Add new rows in table with modifications (Custom classes)
@@ -192,13 +204,13 @@ for tag in tags:
     soup.tbody.append(tag)
 
 
-# In[83]:
+# In[284]:
 
 
 soup.tbody
 
 
-# In[84]:
+# In[285]:
 
 
 #Generate Output Html File
@@ -206,6 +218,12 @@ new_html =soup.contents
 new_html = soup.prettify("utf-8")
 with open("output1.html", "wb") as file:
     file.write(new_html)
+
+
+# In[286]:
+
+
+hasNumbers('2.1')
 
 
 # In[ ]:
