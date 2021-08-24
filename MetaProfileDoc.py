d0e175bdd0b4d5295d4f7344bb61021152ec1308
abdulrehman <abdulrehman.ch@live.com>
2021-08-24 20:24:07 +0500

    - reformatting code and readme update

--- MetaProfileDoc.py
+++ MetaProfileDoc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#Import Necessary Libraries
+# Import Necessary Libraries
 
 from typing import cast
 import numpy as np
@@ -10,9 +10,7 @@ from bs4 import BeautifulSoup
 import argparse
 import sys
 
-
-
-#Row Colors, Different Colors are used for different Levels
+# Row Colors, Different Colors are used for different Levels
 color_RowClass1 = '#77a8a8'
 color_RowClass2 = '#eaece5'
 color_RowClass3 = '#ddeedd'
@@ -20,52 +18,51 @@ color_RowClass4 = '#c0ded9'
 color_RowClass5 = '#3b3a30'
 
 
-#Function that is used to check if the cell value is either number or String
-#Number defines the hierarchy while String means heading.
+# Function that is used to check if the cell value is either number or String
+# Number defines the hierarchy while String means heading.
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
 
 
-#Row with plus sign
-#Example Html to generate that could expand by clicking on (+) sign.
-#expand_next_row = '<tr class="MDrow1 expand-next-row"><td class="MDrow1"><span class="plus-sign">+</span><span class="plus-sign" style="display: none;">–</span>'
+# Row with plus sign
+# Example Html to generate that could expand by clicking on (+) sign.
+# expand_next_row = '<tr class="MDrow1 expand-next-row"><td class="MDrow1"><span class="plus-sign">+</span><span class="plus-sign" style="display: none;">–</span>'
 
-#Example Row next to plus sign
-#expandable_row = '<tr class="expandable-row"><td class="MDrow1">'
+# Example Row next to plus sign
+# expandable_row = '<tr class="expandable-row"><td class="MDrow1">'
 
 
-#Function to get class name that can be used for fomatting( background color, text color exapandable )
-def getClassname(number_of_dec,next_number_of_dec,cellValue):
-
-    #Check if first cell contains number then create hierarchy of rows, If cell contain string then make heading
-    if(hasNumbers(cellValue)):
-        if ((number_of_dec == 0)):                                     #Whole Number
+# Function to get class name that can be used for fomatting( background color, text color exapandable )
+def getClassname(number_of_dec, next_number_of_dec, cellValue):
+    # Check if first cell contains number then create hierarchy of rows, If cell contain string then make heading
+    if (hasNumbers(cellValue)):
+        if ((number_of_dec == 0)):  # Whole Number
             if (number_of_dec < next_number_of_dec):
-                return  '''<tr class="MDrow1 RowClass2 expand-next-row"> 
+                return '''<tr class="MDrow1 RowClass2 expand-next-row"> 
                                 <td class="MDrow1">
                                     <span class="plus-sign">+</span>
                                     <span class="plus-sign" style="display: none;">–</span>'''
-            return  '''<tr class="MDrow RowClass2">
+            return '''<tr class="MDrow RowClass2">
                             <td class="MDrow1">'''
 
-        elif number_of_dec == 1:                                       #One Decimel
-            if (number_of_dec < next_number_of_dec):                   #If Row has Child Rows make it expandable
+        elif number_of_dec == 1:  # One Decimel
+            if (number_of_dec < next_number_of_dec):  # If Row has Child Rows make it expandable
                 return '''<tr class="MDrow1 expandable-row expand-next-row RowClass3">
                             <td class="MDrow1">
                                 <span class="plus-sign" style="display: none;">+ </span>
                                 <span class="plus-sign">– </span>'''
             return '''<tr class="expandable-row RowClass3">
                         <td class="MDrow1">'''
-        elif number_of_dec == 2:                                       #Two Decimel
-            if (number_of_dec < next_number_of_dec):                   #If Row has Child Rows make it expandable
+        elif number_of_dec == 2:  # Two Decimel
+            if (number_of_dec < next_number_of_dec):  # If Row has Child Rows make it expandable
                 return '''<tr class="MDrow1 expandable-row expand-next-row RowClass4">
                             <td class="MDrow1">
                                 <span class="plus-sign" style="display: none;">+ </span>
                                 <span class="plus-sign">– </span>'''
             return '''<tr class="expandable-row RowClass4">
                         <td class="MDrow4">'''
-        elif number_of_dec == 3:                                       #Three Decimel
-            if (number_of_dec < next_number_of_dec):                   #If Row has Child Rows make it expandable
+        elif number_of_dec == 3:  # Three Decimel
+            if (number_of_dec < next_number_of_dec):  # If Row has Child Rows make it expandable
                 return '''<tr class="MDrow1 expandable-row expand-next-row RowClass5">
                             <td class="MDrow1">
                                 <span class="plus-sign" style="display: none;">+ </span>
@@ -74,19 +71,23 @@ def getClassname(number_of_dec,next_number_of_dec,cellValue):
                         <td class="RowClass5">'''
         else:
             return '''<tr class="">'''
-    else:                                                            #Header Row
+    else:  # Header Row
         return f'''<tr class="Header1row RowClass2">
                         <td class="">{cellValue}'''
 
 
 # Initialize parser
 parser = argparse.ArgumentParser()
- 
+
 # Adding optional argument
-parser.add_argument("-html", "--HTML", help = "HTML File complete Path with extension i.e C:\<path>\example=page.html" , required=True)
-parser.add_argument("-excel", "--EXCEL", help = "EXCEL File complete Path with extension i.e C:\<path>\LstMetadataprofiltest_formatted.xlsx" , required=True)
-parser.add_argument('-config', '--CONFIG', help='delimited list input i.e 0,1,33,34,35',  type=lambda s: [int(item) for item in s.split(',')] , required=True)
- 
+parser.add_argument("-html", "--HTML", help="HTML File complete Path with extension i.e C:\<path>\example=page.html",
+                    required=True)
+parser.add_argument("-excel", "--EXCEL",
+                    help="EXCEL File complete Path with extension i.e C:\<path>\LstMetadataprofiltest_formatted.xlsx",
+                    required=True)
+parser.add_argument('-config', '--CONFIG', help='delimited list input i.e 0,1,33,34,35',
+                    type=lambda s: [int(item) for item in s.split(',')], required=True)
+
 # Read arguments from command line
 args = parser.parse_args()
 
@@ -94,7 +95,7 @@ if not args.HTML or not args.EXCEL or not args.CONFIG:
     print('-html and -excel arguments are required, Please see the Readme File')
     sys.exit(1)
 
-#Read orignal html file, for embedding new table
+# Read orignal html file, for embedding new table
 file_path = args.HTML
 with open(file_path) as fp:
     soup = BeautifulSoup(fp, 'html.parser')
@@ -102,74 +103,73 @@ with open(file_path) as fp:
 # soup = soup.find("table",{"class": "mainTable"})
 
 
-#Input File (Excel or CSV) to parse for creating html Table.
+# Input File (Excel or CSV) to parse for creating html Table.
 filename = args.EXCEL
 
 if '.csv' in filename:
-    #Read csv to create html table
-    df = pd.read_csv(filename,header=None,encoding = "ISO-8859-1")
+    # Read csv to create html table
+    df = pd.read_csv(filename, header=None, encoding="ISO-8859-1")
 elif '.xlsx' in filename:
-    #Read Excel File to create html table
-    df = pd.read_excel(filename,header=None,encoding= "ISO-8859-1")
+    # Read Excel File to create html table
+    df = pd.read_excel(filename, header=None, encoding="ISO-8859-1")
 
-df = df[df[len(df.columns)-1] != 1]  #Remove rows based on column values i.e If Last Column (Remove_Rows) value is 1 remove complete row ignore.
+df = df[df[len(
+    df.columns) - 1] != 1]  # Remove rows based on column values i.e If Last Column (Remove_Rows) value is 1 remove complete row ignore.
 # df = df[[0,1,33,34,35]]              #Select the specific columns (including first column that defines the hierarchy) to create table
-df = df[args.CONFIG]              #Select the specific columns (including first column that defines the hierarchy) to create table
-df = df.dropna(how='all')            #Select the empty rows
-df = df.reset_index(drop=True)       #Remove the empty rows
+df = df[args.CONFIG]  # Select the specific columns (including first column that defines the hierarchy) to create table
+df = df.dropna(how='all')  # Select the empty rows
+df = df.reset_index(drop=True)  # Remove the empty rows
 
+# Choose columns with data and ignore the class column
+sub_set_df_fhtml = df[
+    args.CONFIG[1:]]  # Select Columns that contain data excluding first column that contains foramtting info
 
-#Choose columns with data and ignore the class column
-sub_set_df_fhtml = df[args.CONFIG[1:]]     #Select Columns that contain data excluding first column that contains foramtting info
-
-#Replace nan values with " "
+# Replace nan values with " "
 sub_set_df_fhtml = sub_set_df_fhtml.replace(np.nan, '', regex=True)
 df = df.replace(np.nan, '', regex=True)
 
 # Create New Html Table
-html = sub_set_df_fhtml.to_html(index=False,header=False)
+html = sub_set_df_fhtml.to_html(index=False, header=False)
 
-#Create a parser to parse newly created table for adding custom classes
-new_soup = BeautifulSoup(html,"html.parser")
-tags = new_soup.find_all('tr',recursive=True)
+# Create a parser to parse newly created table for adding custom classes
+new_soup = BeautifulSoup(html, "html.parser")
+tags = new_soup.find_all('tr', recursive=True)
 
-
-#Iterate over each row of newly created table and check which class it belongs to and then make necessary changes/formatting
-for num , tag in enumerate(tags):
-    if (num > 0 and (num < len(tags)-1)):
-        #Row Fomatting
-        #print(num)
-        row_class = getClassname(str(df[0][num]).count('.'),str(df[0][num+1]).count('.'),str(df[0][num]))
+# Iterate over each row of newly created table and check which class it belongs to and then make necessary
+# changes/formatting
+for num, tag in enumerate(tags):
+    if num > 0 and (num < len(tags) - 1):
+        # Row Fomatting
+        # print(num)
+        row_class = getClassname(str(df[0][num]).count('.'), str(df[0][num + 1]).count('.'), str(df[0][num]))
         tags[num] = str(tag).replace("<tr>\n<td>", row_class)
         tags[num] = BeautifulSoup(tags[num], 'lxml')
-        tags[num] = tags[num].find_all('tr',recursive=True)[0]
-        #print(tags[num])
-    elif((num < 1)):
-        #Header Formatting
-        #print(num)
-        row_class = getClassname(str(df[0][num]).count('.'),str(df[0][num+1]).count('.'),str(df[0][num]))
+        tags[num] = tags[num].find_all('tr', recursive=True)[0]
+        # print(tags[num])
+    elif num < 1:
+        # Header Formatting
+        # print(num)
+        row_class = getClassname(str(df[0][num]).count('.'), str(df[0][num + 1]).count('.'), str(df[0][num]))
         tags[num] = str(tag).replace("<tr>", '<tr class="Headerrow RowClass1">')
         tags[num] = BeautifulSoup(tags[num], 'lxml')
-        tags[num] = tags[num].find_all('tr',recursive=True)[0]
-        #print(tags[num])
+        tags[num] = tags[num].find_all('tr', recursive=True)[0]
+        # print(tags[num])
 
-
-#remove rows from the existing table to add new rows with formatting
-for tr in new_soup.find_all("tr"): 
+# remove rows from the existing table to add new rows with formatting
+for tr in new_soup.find_all("tr"):
     tr.decompose()
 
-#Add new rows in table generated from Excel with formatting (Custom classes)
+# Add new rows in table generated from Excel with formatting (Custom classes)
 for tag in tags:
     new_soup.tbody.append(tag)
 
-#Replace Existing table of sorce html file with the newly generated table from excel
-soup.find("table",{"class": "mainTable"}).replace_with(new_soup)
+# Replace Existing table of sorce html file with the newly generated table from excel
+soup.find("table", {"class": "mainTable"}).replace_with(new_soup)
 
-
-#Generate Output Html File
-new_html =soup.contents
+# Generate Output Html File
+new_html = soup.contents
 new_html = soup.prettify("utf-8")
 
-#Write changes in html file
+# Write changes in html file
 with open("output1.html", "wb") as file:
     file.write(new_html)
--- Readme.md
+++ Readme.md
@@ -1,30 +1,38 @@
-# Excel To HTML Parser
+# Meta Profile Doc
 
-## This Repository contains python script that can be used to generate HTML table using either xlsx or csv file.
+This Repository contains python script that can be used to generate HTML of Meta data using either xlsx or csv file.
 
-#### Note: It is recommended to use xlsx file for generating tables in order to avoid encoding issues.
+## Dependencies:
 
+- beautifulsoup4
+- numpy
+- pandas
+- argparse
 
-## File paths:
+## How to run the code:
 
-- File paths will be passed as command line arguments with -html flag for html file path and -excel flag for excel file path and -config flag for required columns in the output table. Please see the section "How to run the code" for better understanding of paths. 
+### Necessary Files To Run The Project:
+The project requires three files to run successfully, A Meta Profile Doc html template (`example-page.html`) file, Input Excel file (`LstMetadataprofiltest_formatted.xlsx`) of Meta data and Excel to HTML converter (`MetaProfileDoc.py`). 
 
-- NOTE: If excel and html files are in the same directory then onlu file name can be passed with extension, If the html and excel files are not in the same directory as python script then complete path is required.
+- HTML File : example-page.html -> Meta Profile Doc template.
+- LstMetadataprofiltest_formatted.xlsx -> Input Data sheet for Meta data.
+- MetaProfileDoc.py -> Python script for Excel to Html Parsing.
 
-## Row Colors Formatting:
-- In Python script, Following colors are used for different levels of hierarchy(Rows).
-    - color_RowClass1 = '#77a8a8' , Header Row. This color code is used for headings '#77a8a8'.
-    - color_RowClass2 = '#eaece5' , Parent Row(In Excel file, First cell without decimal). This color code is used '#eaece5'.
-    - color_RowClass3 = '#ddeedd' , In Excel file, First cell with One Decimel. This color code is used '#eaece5'.
-    - color_RowClass4 = '#c0ded9' , In Excel file, First cell with Two Decimel. This color code is used '#c0ded9'.
-    - color_RowClass5 = '#3b3a30' , In Excel file, First cell with Three Decimel. This color code is used '#3b3a30'.
+## Run Command
 
-## Requirements:
+```
+python MetaProfileDoc.py -html example-page.html -excel LstMetadataprofiltest_formatted.xlsx -config 0,1,33,34,35
+```
+Where 0,1,33,34,35 are the required columns, You can pass required column's numbers as comma delimited numbers but 0 is 
+mandatory column because it contains information regarding formatting of rows
+
+## XLSX Formatting Guide:
+
 - The First column should only contain numbers (digits) and decimal points (.). 
 - There are two rules , If a row belongs to list then it should contain some class indication in first column (Information) in Excel file such as (12.1) to be a part of the list, or if it is a heading (heading only contains string in first column not numbers) then it should be before the list.
 - There should be no heading between list items, because it will discontinue the list, The heading should be before the starting of the list.
 
-## Example Of Format:
+## Example Format:
 
 - Below given is an example to define hierarchy, Same format should be used to create table correctly. In the example below 'x' represents row number. However script only check decimal points to format rows accordingly.
 
@@ -36,36 +44,11 @@ x.x (list item two with plus sign because it contains one child)
 x.x.x (child of list item two)
 ```
 
-## Necessary Files To Run The Project:
-
-#### Note: The project requires three files to run successfully, First one is the html file in which the python script will add table. Second one is the Excel or CSV file which python script will use to generate table and the third one is the python script file itself which contains the code for parsing Excel file into Html. An Example related to this repository is given below:
-
-- HTML File : example-page.html -> python scrip will use this file and add the newly generated table into this file.
-- LstMetadataprofiltest_formatted.xlsx -> Python script will use this file to generate table and then replace the newly generated table in the html file.
-- MetaProfileDoc.py -> Python script which contains the code for Excel to Html Parsing.
-
-## Requirements (Dependencies):
-
-- python==3.8.3
-- beautifulsoup4==4.9.1
-- numpy==1.18.5
-- pandas==1.0.5
-- argparse==1.1
-- sys==3.8.3
-
-## How to run the code:
-
-#### Note: If the html and excel file is in the same directory then only the name of the files can be used instead of complete path, For Example,
-
-```
-python MetaProfileDoc.py -html example-page.html -excel LstMetadataprofiltest_formatted.xlsx -config 0,1,33,34,35
-```
-#### Where 0,1,33,34,35 are the required columns, You can pass required column's numbers as comma delimited numbers but 0 is mandatory column because it contains information regarding formatting of rows
-
-#### Note: If the html and excel file is not in the same directory then complete path for the files should provide to run the script, For Example,
-
-```
-python MetaProfileDoc.py --html E:\<path to html file>\example-page.html --excel E:\<path to excel file>\LstMetadataprofiltest_formatted.xlsx -config 0,1,33,34,35
-```
-
-#### 'LstMetadataprofiltest_formatted.xlsx' (Excel File) can be used as a reference to generate tables correctly, However 'LstMetadataprofiltest.csv' contains same data but some errors that should be avoided. You can compare both files to see the difference.
+# Other Output.html Information
+## Row Colors Formatting:
+- In Python script, Following colors are used for different levels of hierarchy(Rows).
+    - color_RowClass1 = '#77a8a8' , Header Row. This color code is used for headings '#77a8a8'.
+    - color_RowClass2 = '#eaece5' , Parent Row(In Excel file, First cell without decimal). This color code is used '#eaece5'.
+    - color_RowClass3 = '#ddeedd' , In Excel file, First cell with One Decimel. This color code is used '#eaece5'.
+    - color_RowClass4 = '#c0ded9' , In Excel file, First cell with Two Decimel. This color code is used '#c0ded9'.
+    - color_RowClass5 = '#3b3a30' , In Excel file, First cell with Three Decimel. This color code is used '#3b3a30'.
